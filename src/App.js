2bbe17539217ccf751a10a90c4ac3b6be54b1913
lewibs <benjaminsl2000@gmail.com>
2022-12-06 13:37:38 -0500

    fixed flashing thing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import TypingText from './TypingText';
 import htmlize from "./functions/htmlize";
 
 const App = () => {
+  //the terminal history
   const [history, setHistory] = useState([]);
+  //the users input
   const [user, setUser] = useState("");
+  //the string that is being typed
+  const [buffer, setBuffer] = useState("hi my name is benjamin\n");
+  //the jsx element that is typing
+  const [typing, setTyping] = useState(undefined);
+  //this is used to keep track of if the user is typing
+  const [isTyping, setIsTyping] = useState(false);
+  //this is used to flash the input box
+  const [showInput, setShowInput] = useState(true);
+
+  let ranChecking = false
+  useEffect(() => {
+    if (ranChecking === false) {
+      let interval;
+
+      function isTypingOn() {
+        setIsTyping(true);
+        clearTimeout(interval);
+      }
+
+      function isTypingOff() {
+        interval = setTimeout(()=>{
+          setIsTyping(false);
+        }, 100);
+      }
+
+      window.addEventListener('keydown', isTypingOn);
+      window.addEventListener('keyup', isTypingOff);
+    }
+    ranChecking = true;
+  }, []);
+
+  useEffect(()=>{
+    if (isTyping) {
+      setShowInput(true);
+    } else {
+      const interval = setInterval(() => {
+        setShowInput(prevState => !prevState);
+      }, 500);
+      // Clean up the interval when the component unmounts
+      return () => clearInterval(interval);
+    }
+  }, [isTyping])
 
   //these are the actions which the user is inputing
   let ranListen = false;
@@ -35,21 +79,28 @@ const App = () => {
     }
   }, [user]);
 
+  useEffect(()=>{
+    setTyping(
+      <TypingText onDone={()=>{
+        setHistory(p=>[...p, htmlize(buffer)]);
+        setTyping(undefined);
+      }}>
+        {buffer}
+      </TypingText>
+    );
+  }, [buffer]);
+
   return (
     <>
       {history}
-      <TypingText onDone={()=>{
-        console.log("done typing");
-      }}>
-        this is benjamin
-      </TypingText>
+      {typing}
       <div style={{display:"flex", flexDirection:"row", alignItems:"end"}}>
         {htmlize(user)}
-        <div style={{
+        {showInput && <div style={{
           height: "1rem",
           width: "10px",
           background: "#4AF626",
-        }}/>
+        }}/>}
       </div>
     </>
   )

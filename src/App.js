f6513d782f734e45f470d6612f1ddaf14ce9432c
lewibs <benjaminsl2000@gmail.com>
2025-03-18 18:40:00 -0400

    update homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 import Home from './Home';
 
-const baseURL = "https://lewibs.github.io/";
+const baseURL = "https://lewibs.github.io/stream";
 const Error = <Terminal>{"Error: 404\nhmmmmmmm... I haven't been here yet.\nTry entering 'help' if you're lost\n"}</Terminal>
 
 const App = () => {
